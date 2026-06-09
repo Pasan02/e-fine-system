51639a135959a5116dc8e8f285150ef04eff3a1a
Pasan02 <147014593+Pasan02@users.noreply.github.com>
2026-06-09 10:15:21 +0530

    basic scaffolding done

---
+++ backend/src/main/java/com/slpolice/trafficfines/TrafficfinesApplication.java
@@ -0,0 +1,13 @@
+package com.slpolice.trafficfines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficfinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficfinesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/slpolice/trafficfines/TrafficfinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slpolice.trafficfines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficfinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
